fd922d7b3a1ff132ebf04ffd197d6c07f95c1c49
DorukBüyüktanır <doruk.buyuktanir11@gmail.com>
2024-07-31 22:16:11 +0300

    updating pathFiles

--- GamePie/TerminalStuff/commands.py
+++ GamePie/TerminalStuff/commands.py
@@ -1,13 +1,18 @@
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from resources.importer import import_json 
 from resources.switch import Switch
 import subprocess
 
+
 #Definening Stuff
-commands = import_json("GamePie\commands.json");
+commands = import_json("GamePie\TerminalStuff\commands.json");
 commandSwitch = Switch({
      
 })
-
 
 
 
